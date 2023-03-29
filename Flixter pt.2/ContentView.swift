583f87cd70736a9d8db9804d3bd55d3fffd3d0e4
Derrick Garcon <dgarconpro@gmail.com>
2023-03-28 20:56:32 -0400

    Initial Commit

---
+++ Flixter pt.2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Flixter pt.2
+//
+//  Created by Derrick on 3/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flixter pt.2/Flixter_pt_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Flixter_pt_2App.swift
+//  Flixter pt.2
+//
+//  Created by Derrick on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Flixter_pt_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
